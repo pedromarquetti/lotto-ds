9d6fe3e560ec3cf36db2fce10f8f8c53595c938b
pedromarquetti <pedro.marquettti@gmail.com>
2022-10-11 10:31:06 -0300

    updated code for easy ranged plot

--- ds.py
+++ ds.py
@@ -3,6 +3,7 @@ from statistics import fmean
 from turtle import title
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import requests as req
 
 URL = "https://redeloteria.com.br/rotinas_01/arquivos_txt/tx_megasena_todos_resultados.txt"
@@ -17,54 +18,58 @@ df[nums] = df[nums].apply(
     pd.to_numeric, errors="coerce", axis=1, downcast="integer")
 
 
-def plot_graphs():
+def all_results(res: int):
 
     # "results" column
     total = df.iloc[:, 1:8]
+    figure, ax = plt.subplots(2, 1)
 
-    # Initialise the subplot function using number of rows and columns
-    figure, ax = plt.subplots(2, 2)
+    x_ultimos = df.iloc[:res, 1: 8]
 
-    dez_ultimos = df.iloc[:9, 1:8]
-    dez_ultimos.plot.box(
-        ax=ax[0][1],
-        title="Últimos 10 resultados",
+    x_ultimos.plot.box(
+        ax=ax[0],
+        title=f"Últimos {res} resultados",
         ylabel="Números sorteados (0-60)",
         grid=True,
     )
 
     # Todos os resultados
     total.plot.box(
-        ax=ax[0][0],
+        ax=ax[1],
         title="Todos os resultados",
         ylabel="Números sorteados (0-60)",
         grid=True,
-
     )
 
+
+def virada(res: int):
+    # Initialise the subplot function using number of rows and columns
+    figure, ax = plt.subplots(2, 1)
+
     # Resultados da virada
-    virada = df[df["Data"].str.match("31/12")]
+    virada = df[df["Data"].str.match("31/12")].iloc[:, 1:8]
 
-    resultados = virada.iloc[:, 1:8]
-
-    dez_ultimos_virada = virada.iloc[:9, 1:8]
-    dez_ultimos_virada.plot.box(
-        ax=ax[1][0],
+    x_ultimos = virada.iloc[:res, 1:8]
+    x_ultimos.plot.box(
+        ax=ax[1],
         grid=True,
-        title="10 ultimos Resultados Mega da Virada",
+        title=f"Últimos {res} Resultados Mega da Virada",
         ylabel="Números sorteados (0-60)",
 
     )
 
-    resultados.plot.box(
-        ax=ax[1][1],
+    virada.plot.box(
+        ax=ax[0],
         grid=True,
         title="Resultados Mega da Virada",
         ylabel="Números sorteados (0-60)",
 
     )
-    figure.suptitle("titulo")
-    plt.savefig("4results")
+
+
+def plot_graphs():
+    all_results(5)
+    virada(5)
     plt.show()
 
 
