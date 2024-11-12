a861e8e26cf668a3acc3e731a26ff69330af3f04
pedromarquetti <pedro.marquettti@gmail.com>
2024-11-12 09:12:44 -0300

    fix get request, added User Agent

--- ds.py
+++ ds.py
@@ -1,18 +1,20 @@
-
-from statistics import fmean
-from turtle import title
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 import requests as req
 
 URL = "https://redeloteria.com.br"
+
 PATH = "/resultados/fc_imprime_relatorios.php?jogo=MegaSena&ordem_sorteio=nao&_=1702507555928"
+
 get = req.get(
-    URL+PATH
+    URL+PATH,
+    headers={
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0"
+    }
 )
+
 df = pd.DataFrame(get.json()["data"], columns=[
-    "Conc", "Data",	"n1", "n2", "n3", "n4", "n5", "n6", "Gan.", "Prêmio","Número de apostas"
+    "Conc", "Data", "n1", "n2", "n3", "n4", "n5", "n6", "Gan.", "Prêmio","Número de apostas"
 ])
 nums = ["Conc",	"n1", "n2", "n3", "n4", "n5", "n6", "Gan."]
 df[nums] = df[nums].apply(
@@ -20,7 +22,6 @@ df[nums] = df[nums].apply(
 
 
 def all_results(res: int):
-
     # "results" column
     total = df.iloc[:, 1:8]
     figure, ax = plt.subplots(2, 1)
